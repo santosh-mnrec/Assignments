7de4570a6bb6b7539f3ad3af51c0f9807cfeb3c8
Santosh <santosh.ksingh03@gmail.com>
2016-03-03 21:43:19 +0530

    Re design the form

---
+++ WcfPublisherSubscriber/WcfClient/MainForm.Designer.cs
@@ -0,0 +1,119 @@
+﻿namespace WcfClient
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.listBox2 = new System.Windows.Forms.ListBox();
+            this.listBox3 = new System.Windows.Forms.ListBox();
+            this.btnRun = new System.Windows.Forms.Button();
+            this.btnOver = new System.Windows.Forms.Button();
+            this.btnWicket = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(22, 117);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(132, 186);
+            this.listBox1.TabIndex = 0;
+            // 
+            // listBox2
+            // 
+            this.listBox2.FormattingEnabled = true;
+            this.listBox2.Location = new System.Drawing.Point(225, 117);
+            this.listBox2.Name = "listBox2";
+            this.listBox2.Size = new System.Drawing.Size(132, 186);
+            this.listBox2.TabIndex = 1;
+            // 
+            // listBox3
+            // 
+            this.listBox3.FormattingEnabled = true;
+            this.listBox3.Location = new System.Drawing.Point(436, 117);
+            this.listBox3.Name = "listBox3";
+            this.listBox3.Size = new System.Drawing.Size(132, 186);
+            this.listBox3.TabIndex = 2;
+            // 
+            // btnRun
+            // 
+            this.btnRun.Location = new System.Drawing.Point(22, 30);
+            this.btnRun.Name = "btnRun";
+            this.btnRun.Size = new System.Drawing.Size(75, 23);
+            this.btnRun.TabIndex = 3;
+            this.btnRun.Text = "Run";
+            this.btnRun.UseVisualStyleBackColor = true;
+            this.btnRun.Click += new System.EventHandler(this.btnRun_Click);
+            // 
+            // btnOver
+            // 
+            this.btnOver.Location = new System.Drawing.Point(225, 30);
+            this.btnOver.Name = "btnOver";
+            this.btnOver.Size = new System.Drawing.Size(75, 23);
+            this.btnOver.TabIndex = 4;
+            this.btnOver.Text = "Over";
+            this.btnOver.UseVisualStyleBackColor = true;
+            this.btnOver.Click += new System.EventHandler(this.btnOver_Click);
+            // 
+            // btnWicket
+            // 
+            this.btnWicket.Location = new System.Drawing.Point(436, 29);
+            this.btnWicket.Name = "btnWicket";
+            this.btnWicket.Size = new System.Drawing.Size(75, 23);
+            this.btnWicket.TabIndex = 5;
+            this.btnWicket.Text = "Wicket";
+            this.btnWicket.UseVisualStyleBackColor = true;
+            this.btnWicket.Click += new System.EventHandler(this.btnWicket_Click);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(632, 321);
+            this.Controls.Add(this.btnWicket);
+            this.Controls.Add(this.btnOver);
+            this.Controls.Add(this.btnRun);
+            this.Controls.Add(this.listBox3);
+            this.Controls.Add(this.listBox2);
+            this.Controls.Add(this.listBox1);
+            this.Name = "MainForm";
+            this.Text = "MainForm";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.ListBox listBox2;
+        private System.Windows.Forms.ListBox listBox3;
+        private System.Windows.Forms.Button btnRun;
+        private System.Windows.Forms.Button btnOver;
+        private System.Windows.Forms.Button btnWicket;
+    }
+}
