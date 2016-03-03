9517e18c0fa29c230913773b1ac1dae8216716c0
Santosh <santosh.ksingh03@gmail.com>
2016-03-03 22:01:30 +0530

    Added datastructure project

---
+++ DataStructure/LinkedList/LinkedList.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinkedList
+{
+    public class LinkedList
+    {
+        private Node _head;
+        private int _count = 0;
+        public void AddFirst(int data)
+        {
+            Node newNode = new Node(data);
+            //List is empty
+            if (_head == null)
+                _head = newNode;
+            else
+            {
+                newNode.Next = _head;
+                _head = newNode;
+
+            }
+           
+        }
+        public Node Head
+        {
+
+            get
+            {
+                return _head;
+            }
+        }
+        public Node Tail
+        {
+
+            get
+            {
+                Node current = _head;
+                while (current.Next!=null)
+                {
+                    current = current.Next;
+                }
+                return current;
+
+            }
+        }
+        public int Count
+        {
+            get
+            {
+                Node current = _head;
+                while (current!=null)
+                {
+                    _count++;
+                    current = current.Next;
+                }
+                return _count;
+
+
+            }
+
+            
+        }
+
+        public void AddToTail(int data)
+        {
+            Node newNode = new Node(data);
+            if (_head == null)
+                _head = newNode;
+
+            else
+            {
+                Node current = _head;
+                while (current.Next!=null)
+                {
+                    current = current.Next;
+                }
+                current.Next = newNode;
+
+            }
+        }
+
+        public override string ToString()
+        {
+            Node current = _head;
+            StringBuilder builder = new StringBuilder();
+            while (current!=null)
+            {
+                builder.Append(current.Data + "->");
+                current = current.Next;
+            }
+            builder.Append("NULL");
+            return builder.ToString();
+        }
+    }
+}
---
+++ DataStructure/LinkedList/Node.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinkedList
+{
+    public class Node
+    {
+        public int Data { get; set; }
+        public Node Next { get; set; }
+        public Node(int data)
+        {
+            this.Data = data;
+        }
+        public override string ToString()
+        {
+            return this.Data.ToString();
+        }
+    }
+}
